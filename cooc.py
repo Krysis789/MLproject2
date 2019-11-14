89fe76c336a65f2cddab5af22a71190b4785ef3b
Perrine Woringer <perrine.woringer@supelec.fr>
2019-11-14 17:03:35 +0100

    follow pipeline 'generating word embeddings'

--- cooc.py
+++ cooc.py
@@ -5,13 +5,13 @@ import pickle
 
 
 def main():
-    with open('vocab.pkl', 'rb') as f:
+    with open('Output/vocab.pkl', 'rb') as f:
         vocab = pickle.load(f)
     vocab_size = len(vocab)
 
     data, row, col = [], [], []
     counter = 1
-    for fn in ['pos_train.txt', 'neg_train.txt']:
+    for fn in ['Datasets/train_pos.txt', 'Datasets/train_neg.txt']:
         with open(fn) as f:
             for line in f:
                 tokens = [vocab.get(t, -1) for t in line.strip().split()]
