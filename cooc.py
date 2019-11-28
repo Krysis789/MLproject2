5b7cebb2113563bdb6e9b92aace5ba3bfb96f78f
Krysis <s.afeian@orange.fr>
2019-11-28 15:18:44 +0100

    full

--- cooc.py
+++ cooc.py
@@ -5,13 +5,13 @@ import pickle
 
 
 def main():
-    with open('Output/vocab.pkl', 'rb') as f:
+    with open('vocab_full.pkl', 'rb') as f:
         vocab = pickle.load(f)
     vocab_size = len(vocab)
 
     data, row, col = [], [], []
     counter = 1
-    for fn in ['Datasets/train_pos.txt', 'Datasets/train_neg.txt']:
+    for fn in ['Datasets/train_pos_full.txt', 'Datasets/train_neg_full.txt']:
         with open(fn) as f:
             for line in f:
                 tokens = [vocab.get(t, -1) for t in line.strip().split()]
@@ -28,7 +28,7 @@ def main():
     cooc = coo_matrix((data, (row, col)))
     print("summing duplicates (this can take a while)")
     cooc.sum_duplicates()
-    with open('cooc.pkl', 'wb') as f:
+    with open('cooc_full.pkl', 'wb') as f:
         pickle.dump(cooc, f, pickle.HIGHEST_PROTOCOL)
 
 
