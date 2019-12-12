90d62ce891f6913ad5a611220b9f68ffc64fa491
Perrine Woringer <perrine.woringer@supelec.fr>
2019-12-12 18:31:02 +0100

    spell correction

--- build_vocab.sh
+++ build_vocab.sh
@@ -2,4 +2,4 @@
 
 # Note that this script uses GNU-style sed. On Mac OS, you are required to first
 #    brew install gnu-sed --with-default-names
-cat 'Processed_data/'$1 'Processed_data/'$2 | sed "s/ /\n/g" | grep -v "^\s*$" | sort | uniq -c > 'Processed_data/'$3
+cat $1 | sed "s/ /\n/g" | grep -v "^\s*$" | sort | uniq -c > $2
